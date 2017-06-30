a933aaa41d883fde0505b1fd591b60cd968c660b
AnandJyrm <anandjyrm@gmail.com>
2017-06-29 23:46:07 -0400

    june 29th update

--- fib.py
+++ fib.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
+"""Script to generate fibonacci sequence upto a maximum value
+or 1000 if no input using generator functions"""
 from sys import argv
 
-#generator function
+
+# generator function
 def fib():
-	#initial steps
-	a = 0
-	b = 1
-	while True:
-		#generator yield step
-		yield a
-		#fibonacci sum and swap step
-		a, b = b, (a + b)
+    """Generator function"""
+    # initial steps
+    a = 0
+    b = 1
+    while True:
+        # generator yield step
+        yield a
+        # fibonacci sum and swap step
+        a, b = b, (a + b)
+
 
 if __name__ == "__main__":
-	#accept input or use 1000 as max fibonacci
-	if len(argv) == 1:
-		n = 1000
-	else:
-		n = int(argv[1])
-	for i in fib():
-		if i > n:
-			break
-		print i,
+    # accept input or use 1000 as max fibonacci
+    if len(argv) == 1:
+        n = 1000
+    else:
+        n = int(argv[1])
+    for i in fib():
+        if i > n:
+            break
+        print i,
--- list.py
+++ list.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
+"""list all files in the path"""
 from sys import argv
 import os
 
+
 def listFiles(input_path):
-	#os.walk generator to recursively list files and directory
-	for root,dirs,files in os.walk(input_path,topdown=False): 
-		for name in files:
-			#combine path with file name
-			print os.path.join(root, name)
+    """os.walk generator to recursively list files and directory"""
+    for root, dirs, files in os.walk(input_path, topdown=False):
+        for name in files:
+            # combine path with file name
+            print os.path.join(root, name)
+
 
 if __name__ == "__main__":
-	#list files from current or input directory path
-	if len(argv) == 1:
-		listFiles(".")
-	else:
-		listFiles(argv[1])
+    # list files from current or input directory path
+    if len(argv) == 1:
+        listFiles(".")
+    else:
+        listFiles(argv[1])
